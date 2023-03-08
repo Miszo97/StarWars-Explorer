799f70d4710600e08d530d6e32bf79fcdeb8fecd
Kol <kol478@gmail.com>
2023-03-08 16:01:20 +0100

    Add a test

--- starwars_data/tests.py
+++ starwars_data/tests.py
@@ -1,3 +1,15 @@
+from unittest import mock
 from django.test import TestCase
+from django.urls import reverse
+from starwars_data.models import Collection
 
-# Create your tests here.
+class HomePageViewTestCase(TestCase):
+    def test_homepage_view(self):
+        """
+        Tests that the homepage view returns a status code of 200 and uses the correct template
+        """
+        Collection.objects.create(file_name="file_name.csv")
+
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "starwars_data/collection_list.html")
