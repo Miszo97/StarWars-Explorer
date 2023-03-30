4ac80b9646a85c5ecc3ef65423aa4c336972b968
kol478 <kol478@gmail.com>
2023-03-30 11:52:40 +0200

    Update urls

--- starwars_explorer/urls.py
+++ starwars_explorer/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
 
 from starwars_data.views import (
-    AggregateData,
-    ColectionView,
+    CollectionDetailView,
+    CollectionView,
     GenerateCollectionView,
-    HomePageView,
 )
 
 urlpatterns = [
-    path("generate", GenerateCollectionView.as_view(), name="generate"),
-    path("", HomePageView.as_view(), name="home"),
-    path("collections/<int:pk>", ColectionView.as_view(), name="collections"),
-    path("collections/<int:pk>/aggregate", AggregateData.as_view(), name="aggregate"),
+    path("api/generate", GenerateCollectionView.as_view(), name="generate"),
+    path("api/collections/<int:pk>", CollectionDetailView.as_view(), name="collection-detail"),
+    path("api/collections", CollectionView.as_view(), name="collection"),
 ]
